b7db21143d04bfff476ba0d582dc6abab95a8c60
muhammad.irhas <muhammad.irhas@emporiadigital.com>
2018-04-26 19:41:45 +0700

    on progress multi linked list

---
+++ multi/multi.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"fmt"
+)
+
+type ParentNode struct {
+	nextP, prevP *ParentNode
+	parentPayload interface{}
+	childList *ChildList
+}
+
+type ChildNode struct {
+	nextC, prevC *ChildNode
+	childPayload interface{}
+}
+
+type ParentList struct {
+	firstP, lastP *ParentNode
+}
+
+type ChildList struct {
+	firstC, lastC *ChildNode
+}
+
+func (l *ParentList) CreateParentList() {
+	l.firstP = nil
+	l.lastP  = nil
+}
+
+func (l *ParentList) InsertFirstParent(payload interface{}) {
+
+	node := &ParentNode {
+		parentPayload: payload,
+		childList: &ChildList {
+			firstC: nil,
+			lastC: nil,
+		},
+	}
+
+	if l.firstP == nil {
+		l.firstP = node
+		l.lastP = node
+	} else {
+		node.nextP = l.firstP
+		l.firstP.prevP = node
+		l.firstP = node
+	}
+}
+
+func (l *ParentList) InsertFirstChild(parent interface{} ,payload interface{}) {
+
+	node := &ChildNode {
+		childPayload: payload,
+	}
+
+	parentNode := l.FindParent(parent)
+	if parentNode.parentPayload != -1 {
+		if parentNode.childList.firstC == nil {
+			parentNode.childList.firstC = node
+			parentNode.childList.lastC = node
+		} else {
+			node.nextC = parentNode.childList.firstC
+			parentNode.childList.firstC.prevC = node
+			parentNode.childList.firstC = node
+		}
+	} else {
+		fmt.Print("No parent found")
+	}
+}
+
+func (l *ParentList) FindParent(payload interface{}) (n *ParentNode) {
+
+	n = &ParentNode {
+		parentPayload: -1,
+	}
+
+	p := l.firstP
+	for p != nil {
+		if p.parentPayload == payload {
+			n = p
+		}
+		p = p.nextP
+	}
+
+	return n
+}
+
+func (l *ParentList) PrintAllParent() {
+
+	p := l.firstP
+	for p != nil {
+		fmt.Print(" | " ,p.parentPayload)
+		p = p.nextP
+	}
+	fmt.Print(" | ")
+}
+
+func (l *ParentList) PrintAll() {
+
+	p := l.firstP
+	for p != nil {
+		fmt.Print(" | ", p.parentPayload , " | ",  " -> ")
+		pc := p.childList.firstC
+		for pc != nil {
+			fmt.Print(" | ", pc.childPayload)
+			pc = pc.nextC
+		}
+		fmt.Println(" | ")
+		p = p.nextP
+	}
+}
+
+func main() {
+	l := &ParentList{}
+	l.CreateParentList()
+	l.InsertFirstParent(1)
+	l.InsertFirstParent(2)
+	l.InsertFirstParent(3)
+
+	l.InsertFirstChild(3, 4)
+	l.InsertFirstChild(3, 5)
+	l.InsertFirstChild(2, 90)
+
+	// l.PrintAllParent()
+	l.PrintAll()
+}
