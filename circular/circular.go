b810584a3ca5313ba5d01bfcdf133ecda0e7df3d
mfathirirhas <fatheer.eerhas@gmail.com>
2018-05-01 11:14:01 +0700

    add circular linked list

--- circular/circular.go
+++ circular/circular.go
@@ -11,43 +11,52 @@ type Node struct {
 
 type List struct {
 	start *Node
+	end *Node
 }
+
+
+// methods
 
 func (l *List) CreateList() {
 	l.start = nil
+	l.end 	= nil
 }
 
 func (l *List) InsertFirst(payload interface{}) {
 
-	node := &Node {
-		payload: payload,
-	}
-
+	node := &Node{payload: payload}
 	if l.start == nil {
 		l.start = node
+		l.end 	= node
+		node.next = node
+		node.prev = node
 	} else {
 		node.next = l.start
-		node.prev = l.start.prev
-		l.start.prev.next = node
 		l.start.prev = node
+		l.start = node
+		p := l.start
+		for p.next != nil {
+			if p == l.end {
+				p.next = l.start
+				break
+			}
+			p = p.next
+		}
 	}
 }
 
-func (l *List) InsertAfter(payload interface{}, searchNode interface{}) {
-
-	node := &Node {
-		payload: payload,
-	}
+func (l *List) InsertAfter(payload interface{}, search interface{}) {
+	
+	node := &Node{payload: payload}
 
 	if l.start == nil {
 		fmt.Println("List is empty")
 	} else {
-		n := l.Find(searchNode)
+		n := l.FindByName(search)
 		if n.payload == -1 {
-			fmt.Println("No item found")
+			fmt.Println("Inserting after '", search ,"' - No data with name '", search ,"'")
 		} else {
 			node.next = n.next
-			node.prev = n
 			n.next.prev = node
 			n.next = node
 		}
@@ -55,31 +64,82 @@ func (l *List) InsertAfter(payload interface{}, searchNode interface{}) {
 }
 
 func (l *List) InsertLast(payload interface{}) {
-
-	node := &Node {
-		payload: payload,
-	}
-
+	
+	node := &Node{payload: payload}
 	if l.start == nil {
 		l.start = node
+		l.end  = node
+		node.next = node
+		node.prev = node
 	} else {
-		node.next = l.start
-		node.prev = l.start.prev
-		l.start.prev.next = node
+		l.end.next = node
 		l.start.prev = node
+		node.prev = l.end
+		node.next = l.start
+		l.end = node
 	}
 }
 
-func (l *List) Find(payload interface{}) (n *Node) {
+func (l *List) DeleteFirst() {
 
-	n = &Node {
+	if l.start == nil {
+		fmt.Println("List is empty")
+	} else {
+		p := l.start
+		l.start = p.next
+		l.start.prev = l.end
+		l.end.next = l.start
+		p.next = nil
+		l.start.prev = nil
+	}
+}
+
+func (l *List) DeleteAfter(search interface{}) {
+
+	if l.start == nil {
+		fmt.Println("List is empty")
+	} else {
+		n := l.FindByName(search)
+		if n.payload == -1 {
+			fmt.Println("Deleting after '", search ,"' - No data with name '", search ,"'")
+		} else {
+			p := n.next
+			n.next = p.next
+			p.next.prev = n
+			p.next = nil
+			p.prev = nil			
+		}
+	}
+}
+
+func (l *List) DeleteLast() {
+
+	if l.start == nil {
+		fmt.Println("List is empty")
+	} else {
+		p := l.end
+		l.end = p.prev
+		l.end.next = l.start
+		l.start.prev = l.end
+		p.prev.next = nil
+		p.prev = nil
+	}
+}
+
+func (l *List) FindByName(search interface{}) (n *Node) {
+
+	n = &Node{
 		payload: -1,
 	}
 
 	p := l.start
 	for p != nil {
-		if p.payload == payload {
+		if p.payload == search {
 			n = p
+			break
+		}
+
+		if p == l.end {
 			break
 		}
 		p = p.next
@@ -88,24 +148,34 @@ func (l *List) Find(payload interface{}) (n *Node) {
 	return n
 }
 
-func (l *List) Print() {
+func (l *List) PrintAll() {
 
 	p := l.start
 	for p != nil {
-		fmt.Print(" | ", p.payload)
+		fmt.Print("| " ,p.payload, " ")
+		if p == l.end{
+			break
+		}
 		p = p.next
 	}
-	fmt.Print(" | ")
+	fmt.Print("|", "\n")
 }
 
 func main() {
-
 	l := &List{}
 	l.CreateList()
-
+ 
+	l.InsertFirst(3)
 	l.InsertFirst(1)
-	l.InsertLast(3)
+	l.InsertLast(4)
+	l.InsertLast(5)
+	l.InsertLast(6)
 	l.InsertAfter(2, 1)
+	l.InsertAfter(2.5, 2)
 
-	l.Print()
+	l.DeleteFirst()
+	l.DeleteAfter(1)
+	l.DeleteLast()
+
+	l.PrintAll()
 }
