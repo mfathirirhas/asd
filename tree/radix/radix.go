03080cd31fa2193182acb0ba5d85c24f45a3d4db
mfathirirhas <fathir.irhas@tokopedia.com>
2019-03-26 21:38:49 +0700

    radix tree: insertion, lookup, and std functions

---
+++ tree/radix/radix.go
@@ -0,0 +1,321 @@
+package radix
+
+import (
+	"sort"
+)
+
+type Node struct {
+	key    string      // define every unique path in the tree
+	val    interface{} // define the value of every key/path
+	edges  Edges       // slice of all the edges each node has
+	isLeaf bool        // is node has value
+}
+
+type Edge struct {
+	label byte  // first char of node key it's pointing to
+	node  *Node // each edge connect to one node
+}
+
+type Edges []*Edge
+
+type Tree struct {
+	root *Node // define the root of tree
+}
+
+func (t *Tree) CreateTree() {
+	t.root = nil
+}
+
+// to order slice
+func (e Edges) Len() int {
+	return len(e)
+}
+
+func (e Edges) Less(i, j int) bool {
+	return e[i].label < e[j].label
+}
+
+func (e Edges) Swap(i, j int) {
+	e[i], e[j] = e[j], e[i]
+}
+
+// end ---
+
+func setNode(key string, val interface{}, edges Edges) *Node {
+	isLeaf := false
+	if val != nil {
+		isLeaf = true
+	}
+	return &Node{
+		key:    key,
+		val:    val,
+		edges:  edges,
+		isLeaf: isLeaf,
+	}
+}
+
+func setEdge(label byte, node *Node) *Edge {
+	return &Edge{
+		label: label,
+		node:  node,
+	}
+}
+
+func (n *Node) updateNode(key string, val interface{}) {
+	if key != "" {
+		n.key = key
+	}
+	if val != nil {
+		n.val = val
+	}
+}
+
+func (e *Edge) updateEdge(label byte, node *Node) {
+	if label != 0 {
+		e.label = label
+	}
+	if node != nil {
+		e.node = node
+	}
+}
+
+// FindPrefix find the prefix match between input key and root key.
+// Return the index of the next to the last char of matched prefix
+func (n *Node) FindPrefix(key string) (pos int) {
+	max := len(key)
+	if nodeKeyLen := len(n.key); nodeKeyLen < max {
+		max = nodeKeyLen
+	}
+	for pos = 0; pos < max; pos++ {
+		if key[pos] != n.key[pos] {
+			break
+		}
+	}
+	return pos
+}
+
+func findIncision(nodeKey, inputKey string, pos int) (matchedKey, nodeKeyDiff, inputKeyDiff string) {
+	if pos > 0 {
+		matchedKey = nodeKey[:pos]
+		if len(nodeKey) == pos && len(inputKey) > pos { // all nodekey in inputkey
+			inputKeyDiff = inputKey[pos:]
+		} else if len(nodeKey) > pos && len(inputKey) == pos { // all inputkey in nodekey
+			nodeKeyDiff = nodeKey[pos:]
+		} else if len(nodeKey) > pos && len(inputKey) > pos { // both have diff
+			nodeKeyDiff = nodeKey[pos:]
+			inputKeyDiff = inputKey[pos:]
+		}
+	}
+	return
+}
+
+// FindEdge find edge of the node based on label
+func (n *Node) findEdge(label byte) *Edge {
+	// this function implementation is from Armon Dadgar - go-radix
+	num := len(n.edges)
+	idx := sort.Search(num, func(i int) bool {
+		return n.edges[i].label >= label
+	})
+	if idx < num && n.edges[idx].label == label {
+		return n.edges[idx]
+	}
+	return nil
+}
+
+// addEdge add edge to a node
+func (n *Node) addEdge(edges ...*Edge) {
+	for _, edge := range edges {
+		n.edges = append(n.edges, edge)
+	}
+	sort.Sort(n.edges)
+}
+
+// checkEdges check if a node has childs(true) or not(false)
+func (n *Node) checkEdges() bool {
+	if len(n.edges) > 0 {
+		return true
+	}
+	return false
+}
+
+// Insert Insert key & val into the tree recursively
+// params:
+// - node 		: current destination for insertion
+// - parentNode	: parent of current node
+// - key  		: string
+// - val  		: any value
+func (t *Tree) insert(node, parentNode *Node, key string, val interface{}) {
+	if node == nil {
+		t.root = setNode(key, val, nil)
+		return
+	} else {
+		pos := node.FindPrefix(key) // match input key with current node
+		p := node                   // pointer for current node
+
+		// if no matched found between node key and input key then it must be root
+		// if root is not empty, then create new empty root and add edge to old and new node
+		// if root is empty, continue insertion on the next matched edge
+		if pos == 0 {
+			if p == t.root {
+				if p.key != "" {
+					edgeP := setEdge(p.key[0], p)
+					newNode := setNode(key, val, nil)
+					newEdge := setEdge(key[0], newNode)
+					t.root = setNode("", nil, nil)
+					t.root.addEdge(edgeP, newEdge)
+					return
+				} else {
+					e := p.findEdge(key[0])
+					if e != nil {
+						t.insert(e.node, p, key, val)
+					} else {
+						newNode := setNode(key, val, nil)
+						newEdge := setEdge(newNode.key[0], newNode)
+						p.addEdge(newEdge)
+						return
+					}
+				}
+			}
+		}
+		matchedKey, nodeKeyDiff, inputKeyDiff := findIncision(p.key, key, pos)
+
+		// remainder on both node key and input key
+		// IF Root: new root and its 2 edge to old root and to the new node from input
+		// IF Not Root: new node and parent of current node point to new node and new node has 2 edge to new node and old node
+		if nodeKeyDiff != "" && inputKeyDiff != "" {
+			if p == t.root {
+				p.key = nodeKeyDiff
+				edgeP := setEdge(p.key[0], p)
+
+				newNode := setNode(inputKeyDiff, val, nil)
+				edgeN := setEdge(newNode.key[0], newNode)
+
+				t.root = setNode(matchedKey, nil, nil)
+				t.root.addEdge(edgeP, edgeN)
+			} else {
+				newMatchedNode := setNode(matchedKey, nil, nil)
+				newEdgeN := setEdge(nodeKeyDiff[0], p)
+
+				parentNode.findEdge(p.key[0]).updateEdge(newMatchedNode.key[0], newMatchedNode)
+
+				p.updateNode(nodeKeyDiff, nil)
+
+				newNode := setNode(inputKeyDiff, val, nil)
+				newEdge := setEdge(newNode.key[0], newNode)
+
+				newMatchedNode.addEdge(newEdgeN, newEdge)
+			}
+			return
+		}
+
+		// remainder on node key only
+		// this will result in new root with only 1 edge to the old root
+		if nodeKeyDiff != "" && inputKeyDiff == "" {
+			if p == t.root {
+				p.key = nodeKeyDiff
+				newEdge := setEdge(p.key[0], p)
+				t.root = setNode(matchedKey, val, nil)
+				t.root.addEdge(newEdge)
+			} else {
+				newMatchedNode := setNode(matchedKey, val, nil)
+				newEdgeN := setEdge(nodeKeyDiff[0], p)
+				newMatchedNode.addEdge(newEdgeN)
+
+				parentNode.findEdge(p.key[0]).updateEdge(newMatchedNode.key[0], newMatchedNode)
+
+				p.updateNode(nodeKeyDiff, nil)
+			}
+			return
+		}
+
+		// remainder on input key only
+		// this will result in checking for current root edge by first indice of input key, if not exist input into new node
+		if nodeKeyDiff == "" && inputKeyDiff != "" {
+			e := p.findEdge(inputKeyDiff[0])
+			if e != nil {
+				t.insert(e.node, p, inputKeyDiff, val)
+			} else {
+				newNode := setNode(inputKeyDiff, val, nil)
+				newEdge := setEdge(newNode.key[0], newNode)
+				p.addEdge(newEdge)
+				return
+			}
+		}
+
+		// if both remainder empty
+		if nodeKeyDiff == "" && inputKeyDiff == "" {
+			if p == t.root {
+				t.root.val = val
+			} else {
+				p.val = val
+			}
+			return
+		}
+	}
+}
+
+func (t *Tree) InsertTree(key string, val interface{}) {
+	t.insert(t.root, nil, key, val)
+}
+
+// LookUp find value by key and if exist also return true
+func (t *Tree) LookUp(node *Node, key string) (interface{}, bool) {
+	var (
+		val     interface{}
+		isExist bool
+	)
+	if node == nil {
+		val, isExist = nil, false
+	} else {
+		pos := node.FindPrefix(key)
+		p := node
+		// if none match
+		if pos == 0 {
+			if p == t.root {
+				if p.key != "" {
+					val, isExist = nil, false
+				} else {
+					e := p.findEdge(key[0])
+					if e != nil {
+						val, isExist = t.LookUp(e.node, key)
+					} else {
+						val, isExist = nil, false
+					}
+				}
+			}
+		}
+		_, nodeKeyDiff, inputKeyDiff := findIncision(p.key, key, pos)
+
+		if nodeKeyDiff != "" && inputKeyDiff != "" {
+			val, isExist = nil, false
+		}
+
+		if nodeKeyDiff != "" && inputKeyDiff == "" {
+			val, isExist = nil, false
+		}
+
+		if nodeKeyDiff == "" && inputKeyDiff != "" {
+			e := p.findEdge(inputKeyDiff[0])
+			if e != nil {
+				val, isExist = t.LookUp(e.node, inputKeyDiff)
+			} else {
+				val, isExist = nil, false
+			}
+		}
+
+		// if both remainder empty
+		if nodeKeyDiff == "" && inputKeyDiff == "" {
+			if p.isLeaf {
+				val, isExist = p.val, true
+			}
+		}
+	}
+	return val, isExist
+}
+
+func (t *Tree) LookUpTree(key string) interface{} {
+	if val, isExist := t.LookUp(t.root, key); isExist {
+		return val
+	}
+	return nil
+}
---
+++ tree/radix/radix_test.go
@@ -0,0 +1,319 @@
+package radix
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestCreateTree(t *testing.T) {
+	tree := &Tree{}
+	tree.CreateTree()
+	if tree.root != nil {
+		t.Error("[TestCreateTree] failed creating new binary tree")
+	}
+}
+
+func TestSetNode(t *testing.T) {
+	tree := &Tree{}
+	tree.root = setNode("abc", 1, nil)
+	if tree.root.key != "abc" || tree.root.val != 1 {
+		t.Error("[TestSetNode] failed setting node into the root")
+	}
+	newNode := setNode("abcdd", 2, nil)
+	if newNode.key != "abcdd" || newNode.val != 2 {
+		t.Error("[TestSetNode] failed setting node")
+	}
+}
+
+func TestSetEdge(t *testing.T) {
+	s := "key"
+	newEdge1 := setEdge(s[0], nil)
+	if newEdge1.label != 'k' {
+		t.Error("[TestSetEdge] failed setting edge")
+	}
+	newEdge2 := setEdge('k', nil)
+	if newEdge2.label != 107 {
+		t.Error("[TestSetEdge] failed setting edge")
+	}
+}
+
+func TestUpdateNode(t *testing.T) {
+	n := &Node{
+		key: "key",
+		val: 1,
+	}
+	n.updateNode("abc", 2)
+	if n.key != "abc" || n.val != 2 {
+		t.Error("[TestUpdateNode] failed updating node")
+	}
+}
+
+func TestUpdateEdge(t *testing.T) {
+	e := &Edge{
+		label: 'k',
+	}
+	e.updateEdge('a', nil)
+	if e.label != 'a' {
+		t.Error("[TestUpdateNode] failed updating edge")
+	}
+}
+
+func TestFindPrefix(t *testing.T) {
+	incisionExpect := 4
+	noIncisionExpect := 0
+	partlyIncisionExpect := 3
+	// both incision
+	n := &Node{key: "romula"}
+	key := "romuda"
+	pos := n.FindPrefix(key)
+	// node key incision
+	n2 := &Node{key: "romula"}
+	key2 := "romu"
+	pos2 := n2.FindPrefix(key2)
+	// input key incision
+	n3 := &Node{key: "romu"}
+	key3 := "romuda"
+	pos3 := n3.FindPrefix(key3)
+	// no incision
+	n4 := &Node{key: "rick"}
+	key4 := "morty"
+	pos4 := n4.FindPrefix(key4)
+	// partly incision
+	n5 := &Node{key: "roma"}
+	key5 := "romuda"
+	pos5 := n5.FindPrefix(key5)
+	if pos != incisionExpect && pos2 != incisionExpect && pos3 != incisionExpect && pos5 != partlyIncisionExpect && pos4 != noIncisionExpect {
+		t.Error("[TestFindPrefix] Error finding prefix")
+	}
+}
+
+func TestFindIncision(t *testing.T) {
+	nodeKey := "romula"
+	inputKey := "romuda"
+	pos := 4
+	matchedKey, nodeKeyDiff, inputKeyDiff := findIncision(nodeKey, inputKey, pos)
+	if matchedKey != "romu" {
+		t.Error("[TestFindIncision] Error invalid matchedKey")
+	}
+	if nodeKeyDiff != "la" {
+		t.Error("[TestFindIncision] Error invalid nodeKeyDiff")
+	}
+	if inputKeyDiff != "da" {
+		t.Error("[TestFindIncision] Error invalid inputKeyDiff")
+	}
+}
+
+func TestFindEdge(t *testing.T) {
+	n1 := &Node{
+		key: "da",
+		val: 123,
+	}
+	n2 := &Node{
+		key: "la",
+		val: 234,
+	}
+	n3 := &Node{
+		key: "wi",
+		val: 345,
+	}
+	e1 := &Edge{
+		label: n1.key[0],
+		node:  n1,
+	}
+	e2 := &Edge{
+		label: n2.key[0],
+		node:  n2,
+	}
+	e3 := &Edge{
+		label: n3.key[0],
+		node:  n3,
+	}
+
+	p := &Node{
+		key:   "roma",
+		val:   0,
+		edges: []*Edge{e1, e2, e3},
+	}
+	findLabel1 := n1.key[0]
+	findLabel2 := n2.key[0]
+	findLabel3 := n3.key[0]
+	idx1 := p.findEdge(findLabel1)
+	if idx1 != e1 {
+		t.Error("[TestFindEdge] Error invalid edge")
+	}
+	idx2 := p.findEdge(findLabel2)
+	if idx2 != e2 {
+		t.Error("[TestFindEdge] Error invalid edge")
+	}
+	idx3 := p.findEdge(findLabel3)
+	if idx3 != e3 {
+		t.Error("[TestFindEdge] Error invalid edge")
+	}
+}
+
+func TestAddEdge(t *testing.T) {
+	n := &Node{
+		key: "abc",
+		val: 1,
+	}
+	kid1 := &Node{
+		key: "abcd",
+		val: 2,
+	}
+	kid2 := &Node{
+		key: "abce",
+		val: 3,
+	}
+	e1 := setEdge('d', kid1)
+	e2 := setEdge('e', kid2)
+	n.addEdge(e1)
+	n.addEdge(e2)
+	if n.edges[0].label != 'd' && n.edges[1].label != 'd' {
+		t.Error("[TestAddEdge] Error invalid edge")
+	}
+}
+
+func TestCheckEdges(t *testing.T) {
+	n := &Node{
+		key: "abc",
+		val: 1,
+	}
+	kid1 := &Node{
+		key: "def",
+		val: 2,
+	}
+	kid2 := &Node{
+		key: "efg",
+		val: 3,
+	}
+	e1 := setEdge(kid1.key[0], kid1)
+	e2 := setEdge(kid2.key[1], kid2)
+	n.edges = append(n.edges, e1)
+	n.edges = append(n.edges, e2)
+	if c := n.checkEdges(); !c {
+		t.Error("[TestCheckEdges] Error should has child")
+	}
+}
+
+func TestInsertTree(t *testing.T) {
+	// test 1
+	fmt.Println("---test 1---")
+	key1, val1 := "romuda", 1
+	key2, val2 := "romula", 2
+	key3, val3 := "romawi", 3
+	key4, val4 := "fizzbuzz", 4
+	tree := &Tree{}
+	tree.CreateTree()
+	tree.InsertTree(key1, val1)
+	tree.InsertTree(key2, val2)
+	tree.InsertTree(key3, val3)
+	tree.InsertTree(key4, val4)
+	fmt.Println(tree.root.key)
+	for i := 0; i < len(tree.root.edges); i++ {
+		fmt.Println(string(tree.root.edges[i].label), " | ", tree.root.edges[i].node.key, " | ", tree.root.edges[i].node.val)
+	}
+	fmt.Println(string(tree.root.edges[1].node.edges[0].label), " | ", tree.root.edges[1].node.edges[0].node.key, " | ", tree.root.edges[1].node.edges[0].node.val)
+	if tree.root.edges[0].label != 'f' || tree.root.edges[0].node.key != "fizzbuzz" || tree.root.edges[0].node.val != 4 {
+		t.Error("Insertion Failed")
+	}
+	if tree.root.key != "" {
+		t.Error("Root should be empty")
+	}
+	if tree.root.edges[1].node.edges[0].label != 'a' || tree.root.edges[1].node.edges[0].node.key != "awi" || tree.root.edges[1].node.edges[0].node.val != 3 {
+		t.Error("Insertion Failed")
+	}
+
+	// test 2
+	fmt.Println("---test 2---")
+	key5, val5 := "romuda", 5
+	key6, val6 := "romusa", 6
+	key7, val7 := "romudala", 7
+	key8, val8 := "romudalax", 8
+	key9, val9 := "romawi", 9
+	tree2 := &Tree{}
+	tree2.CreateTree()
+	tree2.InsertTree(key5, val5)
+	tree2.InsertTree(key6, val6)
+	tree2.InsertTree(key7, val7)
+	tree2.InsertTree(key8, val8)
+	tree2.InsertTree(key9, val9)
+	fmt.Println(tree2.root.key, " | ", tree2.root.val, " | ", tree2.root.isLeaf)
+	fmt.Println(tree2.root.edges[0].node.key, " | ", tree2.root.edges[0].node.val, " | ", tree2.root.edges[0].node.isLeaf)
+	fmt.Println(tree2.root.edges[1].node.key, " | ", tree2.root.edges[1].node.val, " | ", tree2.root.edges[1].node.isLeaf)
+	fmt.Println(tree2.root.edges[1].node.edges[0].node.key, " | ", tree2.root.edges[1].node.edges[0].node.val, " | ", tree2.root.edges[1].node.edges[0].node.isLeaf)
+	fmt.Println(tree2.root.edges[1].node.edges[0].node.edges[0].node.key, " | ", tree2.root.edges[1].node.edges[0].node.edges[0].node.val, " | ", tree2.root.edges[1].node.edges[0].node.edges[0].node.isLeaf)
+	fmt.Println(tree2.root.edges[1].node.edges[0].node.edges[0].node.edges[0].node.key, " | ", tree2.root.edges[1].node.edges[0].node.edges[0].node.edges[0].node.val, " | ", tree2.root.edges[1].node.edges[0].node.edges[0].node.edges[0].node.isLeaf)
+	key10, val10 := "roma", 10
+	tree2.InsertTree(key10, val10)
+	fmt.Println(tree2.root.edges[0].node.key, " | ", tree2.root.edges[0].node.val, " | ", tree2.root.edges[0].node.isLeaf)
+	fmt.Println(tree2.root.edges[0].node.edges[0].node.key, " | ", tree2.root.edges[0].node.edges[0].node.val, " | ", tree2.root.edges[0].node.edges[0].node.isLeaf)
+	if tree2.root.key != "rom" {
+		t.Error("Root should be 'rom'")
+	}
+	if tree2.root.edges[0].label != 'a' || tree2.root.edges[0].node.key != "a" || tree2.root.edges[0].node.val != 10 {
+		t.Error("Insertion Failed")
+	}
+	if tree2.root.edges[1].node.edges[0].node.edges[0].node.key != "la" {
+		t.Error("Insertion Failed")
+	}
+
+	// test 3 for empty root
+	fmt.Println("---test 3---")
+	key11, val11 := "romuda", 10
+	key12, val12 := "romula", 11
+	key13, val13 := "foobar", 12
+	key14, val14 := "foo", 13
+	tree3 := &Tree{}
+	tree3.CreateTree()
+	tree3.InsertTree(key11, val11)
+	tree3.InsertTree(key12, val12)
+	tree3.InsertTree(key13, val13)
+	tree3.InsertTree(key14, val14)
+	fmt.Println("root: ", tree3.root.key)
+	fmt.Println(tree3.root.edges[0].node.key, " | ", tree3.root.edges[0].node.val)
+	fmt.Println(tree3.root.edges[0].node.edges[0].node.key, " | ", tree3.root.edges[0].node.edges[0].node.val)
+	fmt.Println(tree3.root.edges[1].node.key, " | ", tree3.root.edges[1].node.val)
+	fmt.Println(tree3.root.edges[1].node.edges[0].node.key, " | ", tree3.root.edges[1].node.edges[0].node.val)
+	fmt.Println(tree3.root.edges[1].node.edges[1].node.key, " | ", tree3.root.edges[1].node.edges[1].node.val)
+	key15, val15 := "test", 14
+	tree3.InsertTree(key15, val15)
+	fmt.Println(tree3.root.edges[2].node.key, " | ", tree3.root.edges[2].node.val)
+	if tree3.root.edges[0].label != 'f' || tree3.root.edges[0].node.key != "foo" || tree3.root.edges[0].node.val != 13 {
+		t.Error("Insertion Failed")
+	}
+	if tree3.root.edges[1].label != 'r' || tree3.root.edges[1].node.key != "romu" || tree3.root.edges[1].node.val != nil {
+		t.Error("Insertion Failed")
+	}
+	if tree3.root.edges[1].node.edges[0].label != 'd' || tree3.root.edges[1].node.edges[0].node.key != "da" || tree3.root.edges[1].node.edges[0].node.val != 10 {
+		t.Error("Insertion Failed")
+	}
+	if tree3.root.edges[2].label != 't' || tree3.root.edges[2].node.key != "test" || tree3.root.edges[2].node.val != 14 {
+		t.Error("Insertion Failed")
+	}
+}
+
+func TestLookUpTree(t *testing.T) {
+	key5, val5 := "romuda", 5
+	key6, val6 := "romusa", 6
+	key7, val7 := "romudala", 7
+	key8, val8 := "romudalax", 8
+	key9, val9 := "romawi", 9
+	key10, val10 := "roma", 10
+	tree2 := &Tree{}
+	tree2.CreateTree()
+	tree2.InsertTree(key5, val5)
+	tree2.InsertTree(key6, val6)
+	tree2.InsertTree(key7, val7)
+	tree2.InsertTree(key8, val8)
+	tree2.InsertTree(key9, val9)
+	tree2.InsertTree(key10, val10)
+	checkVal5 := tree2.LookUpTree(key5)
+	checkVal6 := tree2.LookUpTree(key6)
+	checkVal7 := tree2.LookUpTree(key7)
+	checkVal8 := tree2.LookUpTree(key8)
+	checkVal9 := tree2.LookUpTree(key9)
+	checkVal10 := tree2.LookUpTree(key10)
+	if checkVal5 != val5 && checkVal6 != val6 && checkVal7 != val7 && checkVal8 != val8 && checkVal9 != val9 && checkVal10 != val10 {
+		t.Error("Error LookUp Values")
+	}
+}
